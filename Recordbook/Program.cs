53bebd295a1515ee8a4cc8a2a0a80d3b020433ef
Arafat Hossan <arafatpjl@gmail.com>
2022-03-03 10:00:00 +1100

    Add Record Book desktop application

---
+++ Recordbook/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using Recordbook.conn;
+
+namespace Recordbook
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Recordbook.Extra.call.DatabaseName = "Recordbook";
+           // Application.Run(new frmsplash());
+
+             Recordbook.conn.Mssqlconnect dc = new conn.Mssqlconnect();
+
+             Application.Run(new frmsplash());
+             
+
+           
+        }
+    }
+}
