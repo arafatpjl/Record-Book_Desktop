b0a9855933ba70bc0f026d8f1faa12f76f076c3a
Arafat Hossan <arafatpjl@gmail.com>
2022-03-19 10:00:00 +1100

    Add Record Book desktop application

---
+++ Recordbook/form/frmNewFileType.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Recordbook.form
+{
+    partial class frmNewFileType
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmNewFileType));
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtfile = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dgv_fileinfo = new System.Windows.Forms.DataGridView();
+            this.btnnew = new System.Windows.Forms.Button();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.btndelete = new System.Windows.Forms.Button();
+            this.btnclose = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_fileinfo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Image = ((System.Drawing.Image)(resources.GetObject("label2.Image")));
+            this.label2.Location = new System.Drawing.Point(-1, 238);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(447, 50);
+            this.label2.TabIndex = 53;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Image = ((System.Drawing.Image)(resources.GetObject("label1.Image")));
+            this.label1.Location = new System.Drawing.Point(-1, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(447, 39);
+            this.label1.TabIndex = 52;
+            this.label1.Text = "New File Type";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtfile
+            // 
+            this.txtfile.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtfile.Location = new System.Drawing.Point(8, 121);
+            this.txtfile.Name = "txtfile";
+            this.txtfile.Size = new System.Drawing.Size(216, 20);
+            this.txtfile.TabIndex = 63;
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(14, 81);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(79, 18);
+            this.label6.TabIndex = 65;
+            this.label6.Text = "File  Type :";
+            // 
+            // dgv_fileinfo
+            // 
+            this.dgv_fileinfo.AllowUserToAddRows = false;
+            this.dgv_fileinfo.AllowUserToDeleteRows = false;
+            this.dgv_fileinfo.BackgroundColor = System.Drawing.SystemColors.MenuBar;
+            this.dgv_fileinfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_fileinfo.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.dgv_fileinfo.Location = new System.Drawing.Point(253, 53);
+            this.dgv_fileinfo.Name = "dgv_fileinfo";
+            this.dgv_fileinfo.ReadOnly = true;
+            this.dgv_fileinfo.Size = new System.Drawing.Size(183, 176);
+            this.dgv_fileinfo.TabIndex = 67;
+            this.dgv_fileinfo.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_fileinfo_cellclick);
+            // 
+            // btnnew
+            // 
+            this.btnnew.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnnew.Image = ((System.Drawing.Image)(resources.GetObject("btnnew.Image")));
+            this.btnnew.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnnew.Location = new System.Drawing.Point(31, 248);
+            this.btnnew.Name = "btnnew";
+            this.btnnew.Size = new System.Drawing.Size(82, 29);
+            this.btnnew.TabIndex = 172;
+            this.btnnew.Text = "    &New";
+            this.btnnew.UseVisualStyleBackColor = true;
+            this.btnnew.Click += new System.EventHandler(this.btnnew_Click);
+            // 
+            // btnsave
+            // 
+            this.btnsave.BackColor = System.Drawing.SystemColors.Control;
+            this.btnsave.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsave.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnsave.Image = ((System.Drawing.Image)(resources.GetObject("btnsave.Image")));
+            this.btnsave.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnsave.Location = new System.Drawing.Point(131, 247);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(92, 31);
+            this.btnsave.TabIndex = 170;
+            this.btnsave.Text = " &Save";
+            this.btnsave.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnsave.UseVisualStyleBackColor = false;
+            this.btnsave.Click += new System.EventHandler(this.btnsave_Click);
+            // 
+            // btndelete
+            // 
+            this.btndelete.BackColor = System.Drawing.SystemColors.Control;
+            this.btndelete.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btndelete.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btndelete.Image = ((System.Drawing.Image)(resources.GetObject("btndelete.Image")));
+            this.btndelete.Location = new System.Drawing.Point(238, 246);
+            this.btndelete.Name = "btndelete";
+            this.btndelete.Size = new System.Drawing.Size(81, 31);
+            this.btndelete.TabIndex = 173;
+            this.btndelete.Text = "&Delete";
+            this.btndelete.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btndelete.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btndelete.UseVisualStyleBackColor = false;
+            this.btndelete.Click += new System.EventHandler(this.btndelete_Click);
+            // 
+            // btnclose
+            // 
+            this.btnclose.BackColor = System.Drawing.Color.Transparent;
+            this.btnclose.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnclose.Image = ((System.Drawing.Image)(resources.GetObject("btnclose.Image")));
+            this.btnclose.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnclose.Location = new System.Drawing.Point(337, 246);
+            this.btnclose.Name = "btnclose";
+            this.btnclose.Size = new System.Drawing.Size(85, 32);
+            this.btnclose.TabIndex = 174;
+            this.btnclose.Text = "   &Close";
+            this.btnclose.UseVisualStyleBackColor = false;
+            this.btnclose.Click += new System.EventHandler(this.btnclose_Click);
+            // 
+            // frmNewFileType
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(445, 286);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnclose);
+            this.Controls.Add(this.btndelete);
+            this.Controls.Add(this.btnnew);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.dgv_fileinfo);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtfile);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmNewFileType";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.frmNewFileType_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_fileinfo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtfile;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView dgv_fileinfo;
+        private System.Windows.Forms.Button btnnew;
+        internal System.Windows.Forms.Button btnsave;
+        internal System.Windows.Forms.Button btndelete;
+        private System.Windows.Forms.Button btnclose;
+    }
+}
---
+++ Recordbook/form/frmNewFileType.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Recordbook.form
+{
+    public partial class frmNewFileType : Form
+    {
+
+        Recordbook.conn.Mssqlconnect dc = new conn.Mssqlconnect();
+        Recordbook.Extra.call com = new Extra.call();
+
+        string value = "";
+
+        DataGridViewRow rowuser;
+
+        public frmNewFileType()
+        {
+            InitializeComponent();
+        }
+
+        private void frmNewFileType_Load(object sender, EventArgs e)
+        {
+            btnnew.Text = "New";
+            btnsave.Text = "Save";
+            btndelete.Enabled = false;
+
+
+            try
+            {
+                string squery = "SELECT * FROM  new_File_Type WHERE sign= 0";
+
+                dgv_fileinfo.DataSource = dc.selectquery(squery);
+                dgv_fileinfo.Columns["FType_ID"].Visible = false;
+                dgv_fileinfo.Columns["sign"].Visible = false;
+                dgv_fileinfo.Columns[1].Width = 150;
+
+            }
+
+            catch
+            {
+
+            }
+
+
+        }
+
+        //private void Close_Click(object sender, EventArgs e)
+        //{
+          
+        //    }
+        
+
+        //private void btnnew_Click(object sender, EventArgs e)
+        //{
+           
+        //}
+
+        //private void btndelete_Click(object sender, EventArgs e)
+        //{
+           
+        //}
+
+        private void dgv_fileinfo_cellclick(object sender, DataGridViewCellEventArgs e)
+        {
+            btnsave.Text = "&Update";
+            btnnew.Text = "Edit";
+            btndelete.Enabled = true;
+
+            int rowindex = e.RowIndex;
+            rowuser = dgv_fileinfo.Rows[rowindex];
+            txtfile.Text = rowuser.Cells["File_Type"].Value.ToString();
+
+        }
+
+        private void btnnew_Click(object sender, EventArgs e)
+        {
+            btndelete.Enabled = false;
+            txtfile.Text = "";
+            if (btnnew.Text == "Edit")
+            {
+                btnnew.Text = "&New";
+                btnsave.Text = "&Save";
+
+            }
+            else
+            {
+                btnsave.Text = "&Update";
+                btnnew.Text = "Edit";
+
+
+            }
+        }
+
+        //private void btnsave_Click(object sender, EventArgs e)
+        //{
+        //    this.Close();
+        //}
+
+        private void btnsave_Click(object sender, EventArgs e)
+        {
+            if (txtfile.Text == "")
+            {
+                MessageBox.Show("Type File_Type");
+                txtfile.Focus();
+                return;
+
+            }
+
+            if (btnsave.Text == "Save")
+            {
+
+
+
+                try
+                {
+                    string squery = "SELECT * FROM  new_File_Type WHERE File_Type= '" + txtfile.Text + "' AND sign= 0 ";
+
+                    dc.selectquery(squery);
+
+                    if (dc.selectquery(squery).Rows.Count > 0)
+                    {
+                        MessageBox.Show("Duplicate File Type");
+                        return;
+                    }
+
+                    else
+                    {
+
+
+                        string query = "INSERT INTO new_File_Type(File_Type) VALUES ('" + txtfile.Text + "')";
+
+                        DialogResult YesNO = MessageBox.Show("Do you want to save ?", "confirmation !", MessageBoxButtons.YesNo);
+                        if (YesNO.ToString() == "Yes")
+                        {
+
+                            if (dc.nonselcetquery(query))
+                            {
+                                MessageBox.Show("DataSaved Successfully", "confirmation !");
+                            }
+                            else
+                            {
+                                MessageBox.Show("Operation failed", "confirmation !");
+                            }
+
+                            txtfile.Text = "";
+                            txtfile.Focus();
+
+                        }
+
+                    }
+
+                }
+
+
+
+
+                catch
+                {
+
+                }
+
+
+
+            }
+            else
+            {
+                if (txtfile.Text == "")
+                {
+                    MessageBox.Show("Type File_Type ");
+                    txtfile.Focus();
+                    return;
+                }
+
+                try
+                {
+                    string squery = "SELECT * FROM  new_File_Type WHERE File_Type= '" + txtfile.Text + "' AND sign= 0";
+
+                    dc.selectquery(squery);
+
+                    if (dc.selectquery(squery).Rows.Count > 0)
+                    {
+                        MessageBox.Show("Duplicate File Type");
+                        return;
+                    }
+
+
+                    string query = "Update new_File_Type set File_Type='" + txtfile.Text + "' where File_Type= '" + rowuser.Cells["File_Type"].Value.ToString() + "'";
+
+                    DialogResult YesNO = MessageBox.Show("Do you want to Update ?", "confirmation !", MessageBoxButtons.YesNo);
+                    if (YesNO.ToString() == "Yes")
+                    {
+
+                        if (dc.nonselcetquery(query))
+                        {
+                            MessageBox.Show("DataUpdate Successfully", "confirmation !");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Operation failed", "confirmation !");
+                        }
+
+                        txtfile.Text = "";
+                        txtfile.Focus();
+
+                    }
+                }
+
+
+
+                catch
+                {
+
+                }
+            }
+        }
+
+        private void btndelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string squery = "update   new_File_Type set sign=1 WHERE File_Type= '" + txtfile.Text + "'";
+
+                DialogResult YesNO = MessageBox.Show("Do you want to Delete ?", "confirmation !", MessageBoxButtons.YesNo);
+                if (YesNO.ToString() == "Yes")
+                {
+                    if (dc.nonselcetquery(squery))
+                    {
+                        MessageBox.Show("DataDelete Successfully", "confirmation !");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Operation failed", "confirmation !");
+                    }
+
+                    txtfile.Text = "";
+                    txtfile.Focus();
+                }
+
+
+            }
+
+
+
+
+            catch
+            {
+
+            }
+        }
+
+        private void btnclose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+}  
+    
+}
