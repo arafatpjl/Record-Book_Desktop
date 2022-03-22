c2e2eeda2d60d4f2b0bccacc4f0461183c420728
Arafat Hossan <arafatpjl@gmail.com>
2022-03-23 10:00:00 +1100

    Add Record Book desktop application

---
+++ Recordbook/form/frmNewPosition.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace Recordbook.form
+{
+    partial class frmNewPosition
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmNewPosition));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSELFNOFROM = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtShelfNoTo = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.OptL = new System.Windows.Forms.RadioButton();
+            this.OptR = new System.Windows.Forms.RadioButton();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.btnclose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Image = ((System.Drawing.Image)(resources.GetObject("label1.Image")));
+            this.label1.Location = new System.Drawing.Point(-1, -2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(458, 51);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Create Position In Cabinet";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Image = ((System.Drawing.Image)(resources.GetObject("label2.Image")));
+            this.label2.Location = new System.Drawing.Point(-1, 265);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(458, 44);
+            this.label2.TabIndex = 18;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(93, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(147, 18);
+            this.label3.TabIndex = 37;
+            this.label3.Text = "Cabinet  No   :";
+            // 
+            // txtSELFNOFROM
+            // 
+            this.txtSELFNOFROM.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtSELFNOFROM.Location = new System.Drawing.Point(151, 114);
+            this.txtSELFNOFROM.Name = "txtSELFNOFROM";
+            this.txtSELFNOFROM.Size = new System.Drawing.Size(80, 20);
+            this.txtSELFNOFROM.TabIndex = 38;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(93, 116);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 18);
+            this.label4.TabIndex = 39;
+            this.label4.Text = "From   :";
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(261, 114);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 18);
+            this.label5.TabIndex = 40;
+            this.label5.Text = "To   :";
+            // 
+            // txtShelfNoTo
+            // 
+            this.txtShelfNoTo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtShelfNoTo.Location = new System.Drawing.Point(306, 112);
+            this.txtShelfNoTo.Name = "txtShelfNoTo";
+            this.txtShelfNoTo.Size = new System.Drawing.Size(85, 20);
+            this.txtShelfNoTo.TabIndex = 41;
+            this.txtShelfNoTo.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(93, 154);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(104, 18);
+            this.label6.TabIndex = 43;
+            this.label6.Text = "Cabinet\'s Side  :";
+            // 
+            // OptL
+            // 
+            this.OptL.AutoSize = true;
+            this.OptL.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OptL.Location = new System.Drawing.Point(98, 195);
+            this.OptL.Name = "OptL";
+            this.OptL.Size = new System.Drawing.Size(47, 17);
+            this.OptL.TabIndex = 44;
+            this.OptL.TabStop = true;
+            this.OptL.Text = "Left";
+            this.OptL.UseVisualStyleBackColor = true;
+            // 
+            // OptR
+            // 
+            this.OptR.AutoSize = true;
+            this.OptR.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OptR.Location = new System.Drawing.Point(176, 195);
+            this.OptR.Name = "OptR";
+            this.OptR.Size = new System.Drawing.Size(55, 17);
+            this.OptR.TabIndex = 45;
+            this.OptR.TabStop = true;
+            this.OptR.Text = "Right";
+            this.OptR.UseVisualStyleBackColor = true;
+            // 
+            // btnsave
+            // 
+            this.btnsave.BackColor = System.Drawing.SystemColors.Control;
+            this.btnsave.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsave.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnsave.Image = ((System.Drawing.Image)(resources.GetObject("btnsave.Image")));
+            this.btnsave.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnsave.Location = new System.Drawing.Point(249, 274);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(78, 31);
+            this.btnsave.TabIndex = 176;
+            this.btnsave.Text = " &Save";
+            this.btnsave.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnsave.UseVisualStyleBackColor = true;
+            this.btnsave.Click += new System.EventHandler(this.btnsave_Click);
+            // 
+            // btnclose
+            // 
+            this.btnclose.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnclose.Image = ((System.Drawing.Image)(resources.GetObject("btnclose.Image")));
+            this.btnclose.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnclose.Location = new System.Drawing.Point(343, 274);
+            this.btnclose.Name = "btnclose";
+            this.btnclose.Size = new System.Drawing.Size(85, 30);
+            this.btnclose.TabIndex = 175;
+            this.btnclose.Text = "   &Close";
+            this.btnclose.UseVisualStyleBackColor = true;
+            this.btnclose.Click += new System.EventHandler(this.btnclose_Click);
+            // 
+            // frmNewPosition
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(456, 308);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.btnclose);
+            this.Controls.Add(this.OptR);
+            this.Controls.Add(this.OptL);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtShelfNoTo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtSELFNOFROM);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmNewPosition";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSELFNOFROM;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtShelfNoTo;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RadioButton OptL;
+        private System.Windows.Forms.RadioButton OptR;
+        internal System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.Button btnclose;
+    }
+}
---
+++ Recordbook/form/frmNewPosition.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Recordbook.form
+{
+    public partial class frmNewPosition : Form
+    {
+
+        Recordbook.conn.Mssqlconnect dc = new conn.Mssqlconnect();
+        Recordbook.Extra.call com = new Extra.call();
+
+        string strp;
+        string strposition;
+        string Optside;
+
+
+        public frmNewPosition()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //private void btnsave_Click(object sender, EventArgs e)
+        // {
+
+        //    
+
+           
+        //}
+
+        private void btnclose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnsave_Click(object sender, EventArgs e)
+        {
+
+            if (txtSELFNOFROM.Text == "")
+            {
+                MessageBox.Show("Type Cabinet No");
+            }
+            if (btnsave.Text == "&Save")
+            {
+                int intsf = int.Parse(txtSELFNOFROM.Text);
+                int intst = int.Parse(txtShelfNoTo.Text);
+
+
+                string query = "";
+                if (OptL.Visible == true)
+                {
+                    Optside = "L";
+                }
+                else
+                {
+                    Optside = "R";
+                }
+                DialogResult YesNO = MessageBox.Show("Do you want to save ?", "confirmation !", MessageBoxButtons.YesNo);
+                //if (YesNO.ToString() == "Yes")
+                for (int i = intsf; i <= intst; i++)
+                {
+                    if ((i > 0) && (i < 10))
+                    {
+                        strp = "CBT" + "00" + i;
+                    }
+                    else
+                    {
+                        strp = "CBT" + "0" + i;
+                    }
+
+                    for (int j = 1; j <= 3; j++)
+                    {
+                        for (int k = 1; k <= 3; k++)
+                        {
+                            strposition = strp + "/" + Optside + "/" + "C" + j + "/" + "R" + k;
+
+                            try
+                            {
+                                query = query + "INSERT INTO new_Self_Position (SELF_NO,SELF_SIDE,SELF_ROW_NO,SELF_COLUMN_NO,SELF_POSITION,DeleteRow,UserName,ComName,EntryDate,EntryTime) VALUES('" + i + "','" + Optside + "','" + j + "','" + k + "','" + strposition + "',0, ' " + Recordbook.Extra.call.UserInfo.Rows[0]["UserName"].ToString() + "','" + com.SysConfigInfo.Rows[0]["ComName"].ToString() + "','" + DateTime.Now.ToString("dd-MMM-yyyy hh:mm:ss") + "','" + DateTime.Now.ToString("hh:mm:ss") + "')";
+                                dc.selectquery(query);
+
+                            }
+
+                            catch
+                            {
+
+                            }
+                        }
+                    }
+                }
+
+                dc.nonselcetquery(query);
+                MessageBox.Show("Data Insert Successfually");
+
+            }
+                 
+            
+
+        }
+
+        
+
+       
+    }
+}
