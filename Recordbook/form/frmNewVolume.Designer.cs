d35c82627d3b15587162320eaaba9e3bb0fe929d
Arafat Hossan <arafatpjl@gmail.com>
2022-03-25 10:00:00 +1100

    Add Record Book desktop application

---
+++ Recordbook/form/frmNewVolume.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Recordbook.form
+{
+    partial class frmNewVolume
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmNewVolume));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtitemname = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dgv_volumeinfo = new System.Windows.Forms.DataGridView();
+            this.btnclose = new System.Windows.Forms.Button();
+            this.btndelete = new System.Windows.Forms.Button();
+            this.btnnew = new System.Windows.Forms.Button();
+            this.btnsave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_volumeinfo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Image = ((System.Drawing.Image)(resources.GetObject("label1.Image")));
+            this.label1.Location = new System.Drawing.Point(-4, -2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(453, 50);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "New Iteam";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Image = ((System.Drawing.Image)(resources.GetObject("label2.Image")));
+            this.label2.Location = new System.Drawing.Point(-4, 311);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(453, 43);
+            this.label2.TabIndex = 17;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtitemname
+            // 
+            this.txtitemname.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtitemname.Location = new System.Drawing.Point(50, 175);
+            this.txtitemname.Name = "txtitemname";
+            this.txtitemname.Size = new System.Drawing.Size(106, 20);
+            this.txtitemname.TabIndex = 37;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(47, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(147, 18);
+            this.label3.TabIndex = 36;
+            this.label3.Text = "Volume No  :";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // dgv_volumeinfo
+            // 
+            this.dgv_volumeinfo.AllowUserToAddRows = false;
+            this.dgv_volumeinfo.AllowUserToDeleteRows = false;
+            this.dgv_volumeinfo.BackgroundColor = System.Drawing.Color.White;
+            this.dgv_volumeinfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_volumeinfo.Location = new System.Drawing.Point(225, 82);
+            this.dgv_volumeinfo.Name = "dgv_volumeinfo";
+            this.dgv_volumeinfo.ReadOnly = true;
+            this.dgv_volumeinfo.Size = new System.Drawing.Size(193, 201);
+            this.dgv_volumeinfo.TabIndex = 52;
+            this.dgv_volumeinfo.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_volumeinfo_CellClick);
+            this.dgv_volumeinfo.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_volumeinfo_CellContentClick);
+            // 
+            // btnclose
+            // 
+            this.btnclose.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnclose.Image = ((System.Drawing.Image)(resources.GetObject("btnclose.Image")));
+            this.btnclose.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnclose.Location = new System.Drawing.Point(333, 319);
+            this.btnclose.Name = "btnclose";
+            this.btnclose.Size = new System.Drawing.Size(85, 31);
+            this.btnclose.TabIndex = 178;
+            this.btnclose.Text = "   &Close";
+            this.btnclose.UseVisualStyleBackColor = true;
+            this.btnclose.Click += new System.EventHandler(this.btnclose_Click);
+            // 
+            // btndelete
+            // 
+            this.btndelete.BackColor = System.Drawing.SystemColors.Control;
+            this.btndelete.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btndelete.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btndelete.Image = ((System.Drawing.Image)(resources.GetObject("btndelete.Image")));
+            this.btndelete.Location = new System.Drawing.Point(234, 319);
+            this.btndelete.Name = "btndelete";
+            this.btndelete.Size = new System.Drawing.Size(81, 31);
+            this.btndelete.TabIndex = 177;
+            this.btndelete.Text = "&Delete";
+            this.btndelete.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btndelete.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btndelete.UseVisualStyleBackColor = true;
+            this.btndelete.Click += new System.EventHandler(this.btndelete_Click);
+            // 
+            // btnnew
+            // 
+            this.btnnew.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnnew.Image = ((System.Drawing.Image)(resources.GetObject("btnnew.Image")));
+            this.btnnew.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnnew.Location = new System.Drawing.Point(23, 319);
+            this.btnnew.Name = "btnnew";
+            this.btnnew.Size = new System.Drawing.Size(82, 29);
+            this.btnnew.TabIndex = 176;
+            this.btnnew.Text = "    &New";
+            this.btnnew.UseVisualStyleBackColor = true;
+            this.btnnew.Click += new System.EventHandler(this.btnnew_Click);
+            // 
+            // btnsave
+            // 
+            this.btnsave.BackColor = System.Drawing.SystemColors.Control;
+            this.btnsave.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsave.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnsave.Image = ((System.Drawing.Image)(resources.GetObject("btnsave.Image")));
+            this.btnsave.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnsave.Location = new System.Drawing.Point(125, 319);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(92, 31);
+            this.btnsave.TabIndex = 175;
+            this.btnsave.Text = " &Save";
+            this.btnsave.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnsave.UseVisualStyleBackColor = true;
+            this.btnsave.Click += new System.EventHandler(this.btnsave_Click);
+            // 
+            // frmNewVolume
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(448, 354);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnclose);
+            this.Controls.Add(this.btndelete);
+            this.Controls.Add(this.btnnew);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.dgv_volumeinfo);
+            this.Controls.Add(this.txtitemname);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmNewVolume";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.frmNewVolume_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_volumeinfo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtitemname;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dgv_volumeinfo;
+        private System.Windows.Forms.Button btnclose;
+        internal System.Windows.Forms.Button btndelete;
+        private System.Windows.Forms.Button btnnew;
+        internal System.Windows.Forms.Button btnsave;
+    }
+}
---
+++ Recordbook/form/frmNewVolume.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Recordbook.form
+{
+    public partial class frmNewVolume : Form
+    {
+
+
+        Recordbook.conn.Mssqlconnect dc = new conn.Mssqlconnect();
+        Recordbook.Extra.call com = new Extra.call();
+
+        string value = "";
+       
+        DataGridViewRow rowuser;
+
+       
+
+
+        public frmNewVolume()
+        {
+            InitializeComponent();
+        }
+
+
+        
+
+
+        private void frmNewVolume_Load(object sender, EventArgs e)
+        {
+            btnnew.Text = "New";
+            btnsave.Text = "Save";
+            btndelete.Enabled = false;
+           
+
+         
+
+            try
+            {
+               string squery = "SELECT * FROM  new_Volume_No WHERE sign= 0 ORDER BY VOLUME_NO ";
+
+               dgv_volumeinfo.DataSource= dc.selectquery(squery);
+               dgv_volumeinfo.Columns["v_ID"].Visible = false;
+               dgv_volumeinfo.Columns["sign"].Visible = false;
+               dgv_volumeinfo.Columns[1].Width = 150;
+
+            }
+
+            catch
+            {
+
+            }
+        }
+
+
+        // save korer jnno
+       
+
+      
+
+
+
+        //public string ne { get; set; }
+
+
+        //Grid Table e Data Add Krer Jnno
+
+        private void dgv_volumeinfo_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            btnsave.Text = "&Update";
+            btnnew.Text = "Edit";
+            btndelete.Enabled = true;
+            
+            int rowindex = e.RowIndex;
+            rowuser = dgv_volumeinfo.Rows[rowindex];
+            txtitemname.Text = rowuser.Cells["VOLUME_NO"].Value.ToString();
+
+        }
+
+      
+
+       
+
+       
+
+      
+        //// Form close Krer Jnno
+        //private void Close_Click(object sender, EventArgs e)
+        //{
+           
+        //}
+
+    // Delete Krer Jnno
+
+        
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgv_volumeinfo_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnclose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btndelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string squery = "update   new_Volume_No set sign=1 WHERE VOLUME_NO= '" + txtitemname.Text + "'";
+
+                DialogResult YesNO = MessageBox.Show("Do you want to Delete ?", "confirmation !", MessageBoxButtons.YesNo);
+                if (YesNO.ToString() == "Yes")
+                {
+                    if (dc.nonselcetquery(squery))
+                    {
+                        MessageBox.Show("DataDelete Successfully", "confirmation !");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Operation failed", "confirmation !");
+                    }
+
+                    txtitemname.Text = "";
+                    txtitemname.Focus();
+                }
+
+
+            }
+
+
+
+
+            catch
+            {
+
+            }
+        }
+
+        private void btnsave_Click(object sender, EventArgs e)
+        {
+            if (txtitemname.Text == "")
+            {
+                MessageBox.Show("Type Volume No");
+                txtitemname.Focus();
+                return;
+            }
+            if (btnsave.Text == "Save")
+            {
+                if (txtitemname.TextLength > 0)
+
+
+
+                    value = "VOL." + " - " + txtitemname.Text;
+
+
+                try
+                {
+                    string squery = "SELECT * FROM  new_Volume_No WHERE VOLUME_NO= '" + value + "' AND sign= 0";
+
+                    dc.selectquery(squery);
+
+                    if (dc.selectquery(squery).Rows.Count > 0)
+                    {
+                        MessageBox.Show("Duplicate File Type");
+                        return;
+                    }
+
+                    else
+                    {
+
+
+                        string query = "INSERT INTO new_Volume_No(VOLUME_NO) VALUES ('" + value + "')";
+
+                        DialogResult YesNO = MessageBox.Show("Do you want to save ?", "confirmation !", MessageBoxButtons.YesNo);
+                        if (YesNO.ToString() == "Yes")
+                        {
+
+                            if (dc.nonselcetquery(query))
+                            {
+                                MessageBox.Show("DataSaved Successfully", "confirmation !");
+                            }
+                            else
+                            {
+                                MessageBox.Show("Operation failed", "confirmation !");
+                            }
+
+                            txtitemname.Text = "";
+                            txtitemname.Focus();
+
+                        }
+
+                    }
+
+                }
+
+
+
+                catch
+                {
+
+                }
+
+            }
+            else
+            {
+                if (txtitemname.Text == "")
+                {
+                    MessageBox.Show("Type Volume No");
+                    txtitemname.Focus();
+                    return;
+                }
+
+                try
+                {
+                    string squery = "SELECT * FROM  new_Volume_No WHERE VOLUME_NO= '" + txtitemname.Text + "' AND sign= 0";
+
+                    dc.selectquery(squery);
+
+                    if (dc.selectquery(squery).Rows.Count > 0)
+                    {
+                        MessageBox.Show("Duplicate File Type");
+                        return;
+                    }
+
+
+                    string query = "Update new_Volume_No set VOLUME_NO='" + txtitemname.Text + "' where VOLUME_NO= '" + rowuser.Cells["VOLUME_NO"].Value.ToString() + "'";
+
+                    DialogResult YesNO = MessageBox.Show("Do you want to Upadte ?", "confirmation !", MessageBoxButtons.YesNo);
+                    if (YesNO.ToString() == "Yes")
+                    {
+
+                        if (dc.nonselcetquery(query))
+                        {
+                            MessageBox.Show("DataUpdate Successfully", "confirmation !");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Operation failed", "confirmation !");
+                        }
+
+                        txtitemname.Text = "";
+                        txtitemname.Focus();
+
+                    }
+                }
+
+
+
+                catch
+                {
+
+                }
+            }
+        }
+
+        private void btnnew_Click(object sender, EventArgs e)
+        {
+            btndelete.Enabled = false;
+            txtitemname.Text = "";
+            if (btnnew.Text == "Edit")
+            {
+                btnnew.Text = "New";
+                btnsave.Text = "Save";
+
+            }
+            else
+            {
+                btnsave.Text = "&Update";
+                btnnew.Text = "Edit";
+
+
+            }
+        }
+
+        }
+
+       
+    }
+
+
