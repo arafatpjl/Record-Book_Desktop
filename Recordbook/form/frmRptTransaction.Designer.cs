916b4f8f580fbebd1bcde913b2f616e55db1803c
Arafat Hossan <arafatpjl@gmail.com>
2022-04-05 10:00:00 +1100

    Add Record Book desktop application

---
+++ Recordbook/form/frmRptTransaction.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace Recordbook.form
+{
+    partial class frmRptTransaction
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmRptTransaction));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtToDate = new System.Windows.Forms.TextBox();
+            this.txtfromDate = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.optDate = new System.Windows.Forms.RadioButton();
+            this.optAll = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnclose = new System.Windows.Forms.Button();
+            this.btnpreview = new System.Windows.Forms.Button();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Image = ((System.Drawing.Image)(resources.GetObject("label1.Image")));
+            this.label1.Location = new System.Drawing.Point(-1, -2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(458, 53);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Report : Transaction Details";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Image = ((System.Drawing.Image)(resources.GetObject("label2.Image")));
+            this.label2.Location = new System.Drawing.Point(-1, 265);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(458, 46);
+            this.label2.TabIndex = 18;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(85, 25);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 18);
+            this.label6.TabIndex = 43;
+            this.label6.Text = "From  :";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtToDate);
+            this.groupBox2.Controls.Add(this.txtfromDate);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(39, 142);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(373, 95);
+            this.groupBox2.TabIndex = 70;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Date";
+            // 
+            // txtToDate
+            // 
+            this.txtToDate.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtToDate.Location = new System.Drawing.Point(193, 57);
+            this.txtToDate.Name = "txtToDate";
+            this.txtToDate.Size = new System.Drawing.Size(146, 20);
+            this.txtToDate.TabIndex = 183;
+            this.txtToDate.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtToDate.TextChanged += new System.EventHandler(this.txtToDate_TextChanged);
+            this.txtToDate.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtToDate_KeyPress);
+            this.txtToDate.Leave += new System.EventHandler(this.txtToDate_Leave);
+            // 
+            // txtfromDate
+            // 
+            this.txtfromDate.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtfromDate.Location = new System.Drawing.Point(26, 57);
+            this.txtfromDate.Name = "txtfromDate";
+            this.txtfromDate.Size = new System.Drawing.Size(146, 20);
+            this.txtfromDate.TabIndex = 182;
+            this.txtfromDate.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtfromDate.TextChanged += new System.EventHandler(this.txtfromDate_TextChanged);
+            this.txtfromDate.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtfromDate_KeyPress);
+            this.txtfromDate.Leave += new System.EventHandler(this.txtfromDate_Leave);
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(268, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 18);
+            this.label3.TabIndex = 44;
+            this.label3.Text = "To  :";
+            // 
+            // optDate
+            // 
+            this.optDate.AutoSize = true;
+            this.optDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.optDate.Location = new System.Drawing.Point(56, 22);
+            this.optDate.Name = "optDate";
+            this.optDate.Size = new System.Drawing.Size(84, 17);
+            this.optDate.TabIndex = 0;
+            this.optDate.Text = "Date Wise";
+            this.optDate.UseVisualStyleBackColor = true;
+            this.optDate.Click += new System.EventHandler(this.optDate_Click);
+            // 
+            // optAll
+            // 
+            this.optAll.AutoSize = true;
+            this.optAll.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.optAll.Location = new System.Drawing.Point(222, 19);
+            this.optAll.Name = "optAll";
+            this.optAll.Size = new System.Drawing.Size(39, 17);
+            this.optAll.TabIndex = 1;
+            this.optAll.Text = "All";
+            this.optAll.UseVisualStyleBackColor = true;
+            this.optAll.Click += new System.EventHandler(this.optAll_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.groupBox1.Controls.Add(this.optAll);
+            this.groupBox1.Controls.Add(this.optDate);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(39, 60);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(373, 53);
+            this.groupBox1.TabIndex = 69;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Criteria";
+            // 
+            // btnclose
+            // 
+            this.btnclose.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnclose.Image = ((System.Drawing.Image)(resources.GetObject("btnclose.Image")));
+            this.btnclose.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnclose.Location = new System.Drawing.Point(310, 273);
+            this.btnclose.Name = "btnclose";
+            this.btnclose.Size = new System.Drawing.Size(85, 30);
+            this.btnclose.TabIndex = 181;
+            this.btnclose.Text = "   &Close";
+            this.btnclose.UseVisualStyleBackColor = true;
+            this.btnclose.Click += new System.EventHandler(this.btnclose_Click);
+            // 
+            // btnpreview
+            // 
+            this.btnpreview.BackColor = System.Drawing.SystemColors.Control;
+            this.btnpreview.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnpreview.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnpreview.Image = ((System.Drawing.Image)(resources.GetObject("btnpreview.Image")));
+            this.btnpreview.Location = new System.Drawing.Point(197, 274);
+            this.btnpreview.Name = "btnpreview";
+            this.btnpreview.Size = new System.Drawing.Size(91, 30);
+            this.btnpreview.TabIndex = 180;
+            this.btnpreview.Text = "&Preview";
+            this.btnpreview.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnpreview.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnpreview.UseVisualStyleBackColor = true;
+            this.btnpreview.Click += new System.EventHandler(this.btnpreview_Click);
+            // 
+            // frmRptTransaction
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(455, 311);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnclose);
+            this.Controls.Add(this.btnpreview);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmRptTransaction";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.frmRptTransaction_Load);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton optDate;
+        private System.Windows.Forms.RadioButton optAll;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnclose;
+        internal System.Windows.Forms.Button btnpreview;
+        private System.Windows.Forms.TextBox txtToDate;
+        private System.Windows.Forms.TextBox txtfromDate;
+    }
+}
---
+++ Recordbook/form/frmRptTransaction.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Recordbook.Report;
+using System.Globalization;
+using CrystalDecisions.CrystalReports.Engine;
+
+namespace Recordbook.form
+{
+    public partial class frmRptTransaction : Form
+    {
+
+        Recordbook.conn.Mssqlconnect dc = new conn.Mssqlconnect();
+        Recordbook.Extra.call com = new Extra.call();
+        string query = "";
+        DataTable dt;
+
+     
+
+
+        public frmRptTransaction()
+        {
+            InitializeComponent();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+       
+        private DataTable previewdata()
+        {
+            if (optDate.Checked == true)
+            {
+                query = "Select * from View_File_Transaction_Details_User Where (Send_Date Between '"
+                    + txtfromDate.Text + "' and '" + txtToDate.Text + "') and compid='" + com.CompanyInfo.Rows[0]["CompID"].ToString() + "' AND USERID='" + Recordbook.Extra.call.UserInfo.Rows[0]["UserID"].ToString() + "' order by trans_no";
+                dt = dc.selectquery(query);
+            }
+            else
+            {
+                query = "Select * from View_File_Transaction_Details_User Where compid='" + com.CompanyInfo.Rows[0]["CompID"].ToString() + "' AND USERID='" + Recordbook.Extra.call.UserInfo.Rows[0]["UserID"].ToString() + "' order by trans_no";
+                dt = dc.selectquery(query);
+          
+            }
+            return dt;
+        }
+
+        private void frmRptTransaction_Load(object sender, EventArgs e)
+        {
+
+            //optDate.Checked = false;
+            txtfromDate.Text = "";
+            txtToDate.Text = "";
+            txtfromDate.Enabled = false;
+            txtToDate.Enabled = false;
+
+
+        }
+
+        private void optDate_Click(object sender, EventArgs e)
+        {
+            txtfromDate.Enabled = true;
+            txtToDate.Enabled = true;
+        }
+
+        private void optAll_Click(object sender, EventArgs e)
+        {
+            txtfromDate.Enabled = false;
+            txtToDate.Enabled = false;
+
+        }
+
+        private void btnclose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnpreview_Click(object sender, EventArgs e)
+        {
+            if (optDate.Checked == true)
+            {
+                if (txtfromDate.Text.Length == 0)
+                {
+                    MessageBox.Show("Type From Date");
+                    txtfromDate.Focus();
+                    return;
+                }
+                if (txtToDate.Text.Length == 0)
+                {
+                    MessageBox.Show("Type To Date");
+                    txtToDate.Focus();
+                    return;
+                }
+
+                ReportViwer RP;
+                DataTable dt = new DataTable();
+                dt = previewdata();
+                RP = new ReportViwer("record Transaction details", dt);
+                RP.Show();
+
+            }
+            else
+            {
+                ReportViwer RP;
+                DataTable dt = new DataTable();
+                dt = previewdata();
+                RP = new ReportViwer("record Transaction details2", dt);
+                RP.Show();
+            }
+
+        }
+
+        private void txtfromDate_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void txtToDate_TextChanged(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void txtfromDate_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+                e.Handled = true;
+
+            this.keyDownNext(sender, e);
+
+        }
+
+        private void txtToDate_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+                e.Handled = true;
+
+            this.keyDownNext(sender, e);
+
+        }
+
+        public void keyDownNext(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                this.SelectNextControl(this.ActiveControl, true, true, true, true);
+            }
+        }
+
+        private void txtfromDate_Leave(object sender, EventArgs e)
+        {
+            var myTxt = (TextBox)sender;
+
+            string date = myTxt.Text;
+            if (myTxt.Text.Length < 6)
+            {
+                MessageBox.Show("Invalid Date.Please 6 digit");
+                myTxt.Focus();
+                return;
+            }
+            if (date.Length == 6)
+            {
+
+                string[] str = new string[3];
+                string strDate = "";
+
+                str[0] = date.Substring(0, 2);
+                str[1] = date.Substring(2, 2);
+                str[2] = "20" + date.Substring(4, 2);  //work from 2000
+
+
+                strDate = str[0] + "-" + str[1] + "-" + str[2];
+                DateTime newdate;
+                if (DateTime.TryParse(strDate, new CultureInfo("en-CA"), DateTimeStyles.None, out newdate))
+                {
+                    myTxt.Text = newdate.ToString("dd-MMM-yyyy");
+                }
+                else
+                    MessageBox.Show("You enter an invalid date.Please enter date as \"DDMMYY\"");
+            }
+
+        }
+
+        private void txtToDate_Leave(object sender, EventArgs e)
+        {
+
+            var myTxt = (TextBox)sender;
+
+            string date = myTxt.Text;
+            if (myTxt.Text.Length < 6)
+            {
+                MessageBox.Show("Invalid Date.Please 6 digit");
+                myTxt.Focus();
+                return;
+            }
+                if (date.Length == 6)
+                {
+
+                    string[] str = new string[3];
+                    string strDate = "";
+
+                    str[0] = date.Substring(0, 2);
+                    str[1] = date.Substring(2, 2);
+                    str[2] = "20" + date.Substring(4, 2);  //work from 2000
+
+
+                    strDate = str[0] + "-" + str[1] + "-" + str[2];
+                    DateTime newdate;
+                    if (DateTime.TryParse(strDate, new CultureInfo("en-CA"), DateTimeStyles.None, out newdate))
+                    {
+                        myTxt.Text = newdate.ToString("dd-MMM-yyyy");
+                    }
+                    else
+                        MessageBox.Show("You enter an invalid date.Please enter date as \"DDMMYY\"");
+                }
+             
+        }
+
+
+    }
+
+       
+    }
+
