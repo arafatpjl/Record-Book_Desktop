8be9fffdfeb90a7a1cfbdcefb8612c219122e27a
Arafat Hossan <arafatpjl@gmail.com>
2022-04-11 10:00:00 +1100

    Add Record Book desktop application

---
+++ Recordbook/form/frmSearching.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace Recordbook.form
+{
+    partial class frmSearching
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmSearching));
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem3 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem4 = new System.Windows.Forms.ListViewItem("");
+            this.label6 = new System.Windows.Forms.Label();
+            this.cmbdept = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtitemname = new System.Windows.Forms.TextBox();
+            this.cmbdesg = new System.Windows.Forms.ComboBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader11 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader9 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader10 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnclose = new System.Windows.Forms.Button();
+            this.btnfind = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 63);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(98, 18);
+            this.label6.TabIndex = 72;
+            this.label6.Text = "Department  :";
+            // 
+            // cmbdept
+            // 
+            this.cmbdept.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbdept.FormattingEnabled = true;
+            this.cmbdept.Location = new System.Drawing.Point(12, 91);
+            this.cmbdept.Name = "cmbdept";
+            this.cmbdept.Size = new System.Drawing.Size(200, 21);
+            this.cmbdept.TabIndex = 71;
+            this.cmbdept.SelectedIndexChanged += new System.EventHandler(this.cmbdept_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Image = ((System.Drawing.Image)(resources.GetObject("label2.Image")));
+            this.label2.Location = new System.Drawing.Point(-2, 363);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(495, 49);
+            this.label2.TabIndex = 68;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Image = ((System.Drawing.Image)(resources.GetObject("label1.Image")));
+            this.label1.Location = new System.Drawing.Point(-2, -4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(495, 53);
+            this.label1.TabIndex = 67;
+            this.label1.Text = "New File Type";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(304, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 18);
+            this.label3.TabIndex = 74;
+            this.label3.Text = "File Nmae  :";
+            // 
+            // txtitemname
+            // 
+            this.txtitemname.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtitemname.Location = new System.Drawing.Point(12, 128);
+            this.txtitemname.Name = "txtitemname";
+            this.txtitemname.Size = new System.Drawing.Size(200, 20);
+            this.txtitemname.TabIndex = 75;
+            this.txtitemname.TextChanged += new System.EventHandler(this.txtitemname_TextChanged);
+            // 
+            // cmbdesg
+            // 
+            this.cmbdesg.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbdesg.FormattingEnabled = true;
+            this.cmbdesg.Location = new System.Drawing.Point(260, 93);
+            this.cmbdesg.Name = "cmbdesg";
+            this.cmbdesg.Size = new System.Drawing.Size(220, 21);
+            this.cmbdesg.TabIndex = 76;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader11,
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7,
+            this.columnHeader8,
+            this.columnHeader9,
+            this.columnHeader10});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1,
+            listViewItem2,
+            listViewItem3,
+            listViewItem4});
+            this.listView1.Location = new System.Drawing.Point(12, 162);
+            this.listView1.Name = "listView1";
+            this.listView1.RightToLeftLayout = true;
+            this.listView1.Size = new System.Drawing.Size(472, 195);
+            this.listView1.TabIndex = 78;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader11
+            // 
+            this.columnHeader11.Text = "ID";
+            this.columnHeader11.Width = 27;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "File Name";
+            this.columnHeader1.Width = 79;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "File No";
+            this.columnHeader2.Width = 107;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "File Type";
+            this.columnHeader3.Width = 85;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Volume No";
+            this.columnHeader4.Width = 80;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Department";
+            this.columnHeader5.Width = 85;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Shelf Position";
+            this.columnHeader6.Width = 100;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Start Date";
+            this.columnHeader7.Width = 100;
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "End Date";
+            this.columnHeader8.Width = 100;
+            // 
+            // columnHeader9
+            // 
+            this.columnHeader9.Text = "Maintainer";
+            this.columnHeader9.Width = 80;
+            // 
+            // columnHeader10
+            // 
+            this.columnHeader10.Text = "Buyer Name";
+            this.columnHeader10.Width = 80;
+            // 
+            // btnclose
+            // 
+            this.btnclose.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnclose.Image = ((System.Drawing.Image)(resources.GetObject("btnclose.Image")));
+            this.btnclose.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnclose.Location = new System.Drawing.Point(352, 374);
+            this.btnclose.Name = "btnclose";
+            this.btnclose.Size = new System.Drawing.Size(89, 31);
+            this.btnclose.TabIndex = 170;
+            this.btnclose.Text = "   &Close";
+            this.btnclose.UseVisualStyleBackColor = true;
+            this.btnclose.Click += new System.EventHandler(this.btnclose_Click);
+            // 
+            // btnfind
+            // 
+            this.btnfind.BackColor = System.Drawing.SystemColors.Control;
+            this.btnfind.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnfind.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnfind.Image = ((System.Drawing.Image)(resources.GetObject("btnfind.Image")));
+            this.btnfind.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnfind.Location = new System.Drawing.Point(324, 125);
+            this.btnfind.Name = "btnfind";
+            this.btnfind.Size = new System.Drawing.Size(78, 27);
+            this.btnfind.TabIndex = 171;
+            this.btnfind.Text = " &Search";
+            this.btnfind.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnfind.UseVisualStyleBackColor = true;
+            this.btnfind.Click += new System.EventHandler(this.btnfind_Click);
+            // 
+            // frmSearching
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(493, 410);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnfind);
+            this.Controls.Add(this.btnclose);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.cmbdesg);
+            this.Controls.Add(this.txtitemname);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cmbdept);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmSearching";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.frmSearching_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cmbdept;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtitemname;
+        private System.Windows.Forms.ComboBox cmbdesg;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+        private System.Windows.Forms.ColumnHeader columnHeader9;
+        private System.Windows.Forms.ColumnHeader columnHeader10;
+        private System.Windows.Forms.ColumnHeader columnHeader11;
+        private System.Windows.Forms.Button btnclose;
+        internal System.Windows.Forms.Button btnfind;
+    }
+}
---
+++ Recordbook/form/frmSearching.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Recordbook.form
+{
+    public partial class frmSearching : Form
+    {
+
+        Recordbook.conn.Mssqlconnect dc = new conn.Mssqlconnect();
+        Recordbook.Extra.call com = new Extra.call();
+
+
+        DataTable dt = null;
+
+        public frmSearching()
+        {
+            InitializeComponent();
+        }
+
+        private void frmSearching_Load(object sender, EventArgs e)
+        {
+
+            try
+            {
+              
+                
+                string squery = "SELECT DeptName,DeptId FROM Dept_Name ";
+   
+                cmbdept.DataSource = dc.selectquery(squery);
+                cmbdept.ValueMember = "DeptId";
+                cmbdept.DisplayMember = "DeptName";
+                cmbdept.Text = "";
+               //this.cmbdept.SelectedIndexChanged += new System.EventHandler(this.cmbdept_SelectedIndexChanged);
+                
+               
+
+            }
+
+            catch
+            {
+
+            }
+
+        }
+
+        private void btnedit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void cmbdept_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            try
+            {
+                string query = "SELECT distinct Dept_Name.DeptName, File_Info_Sub.File_Name  FROM File_Info_Main INNER JOIN Dept_Name ON File_Info_Main.Dept_ID = Dept_Name.DeptId INNER JOIN File_Info_Sub ON File_Info_Main.F_ID = File_Info_Sub.F_ID  where Dept_Name.DeptName='" +cmbdept.Text+ "' and File_Info_Main.deleterow=0 and File_Info_Sub.deleterow=0";
+                cmbdesg.DataSource = dc.selectquery(query);
+                cmbdesg.ValueMember = "File_Name";
+                cmbdesg.DisplayMember = "File_Name";
+                cmbdesg.Text = "";
+                this.cmbdesg.SelectedIndexChanged += new System.EventHandler(this.cmbdesg_SelectedIndexChanged);
+            }
+
+            catch
+            {
+            }
+
+        }
+
+        private void cmbdesg_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+        private void txtitemname_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                listView1.Items.Clear();
+                string query = "select * from View_File_Details_USER Where File_No Like ('" + txtitemname.Text + "%') AND UserId='" + Recordbook.Extra.call.UserInfo.Rows[0]["UserID"].ToString() + "' AND dbo.View_File_Details_USER.CompId='" + com.CompanyInfo.Rows[0]["CompID"].ToString() + "' ";
+                dt = dc.selectquery(query);
+                int a = dt.Rows.Count;
+                for (int i = 0; i <= a - 1; i++)
+                {
+                    ListViewItem lvwFA1er = new ListViewItem(Convert.ToString(i + 1));
+                    lvwFA1er.SubItems.Add(dt.Rows[i]["File_Name"].ToString());
+                    lvwFA1er.SubItems.Add(dt.Rows[i]["File_No"].ToString());
+                    lvwFA1er.SubItems.Add(dt.Rows[i]["File_Type"].ToString());
+                    lvwFA1er.SubItems.Add(dt.Rows[i]["VOLUME_NO"].ToString());
+                    lvwFA1er.SubItems.Add(dt.Rows[i]["DeptName"].ToString());
+                    lvwFA1er.SubItems.Add(dt.Rows[i]["SELF_POSITION"].ToString());
+                    lvwFA1er.SubItems.Add(dt.Rows[i]["Start_Date"].ToString());
+                    lvwFA1er.SubItems.Add(dt.Rows[i]["End_Date"].ToString());
+                    lvwFA1er.SubItems.Add(dt.Rows[i]["MName"].ToString());
+                    lvwFA1er.SubItems.Add(dt.Rows[i]["Buyer_Name"].ToString());
+                    listView1.Items.Add(lvwFA1er);
+
+                }
+            }
+
+            catch
+            {
+            }
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnclose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnfind_Click(object sender, EventArgs e)
+        {
+             if (cmbdept.Text == "")
+            {
+                MessageBox.Show("Select Department Name");
+                cmbdept.Focus();
+                return;
+            }
+
+
+            if (cmbdesg.Text == "")
+            {
+                MessageBox.Show("Select File Name");
+                cmbdesg.Focus();
+                return;
+            }
+            try
+            {
+                listView1.Items.Clear();
+                //string abc = "select * from View_File_Details where DeptName='" + cmbdept.Text + "'  and File_name='" + cmbdesg.Text + "' and UserId='" + Recordbook.Extra.call.UserInfo.Rows[0]["UserID"].ToString() + "' ";
+                string abc = "select * from View_File_Details where DeptName='" + cmbdept.Text + "'  and File_name='" + cmbdesg.Text + "' ";
+                dt = dc.selectquery(abc);
+                int a = dt.Rows.Count;
+                for (int i = 0; i <= a - 1; i++)
+                {
+                    ListViewItem lvwFA1er = new ListViewItem(Convert.ToString(i + 1));
+                    //lvwFA1er.SubItems.Add(dt.Rows[i][""].ToString());
+                    lvwFA1er.SubItems.Add( dt.Rows[i]["File_Name"].ToString());
+                    lvwFA1er.SubItems.Add(dt.Rows[i]["File_No"].ToString());
+                    lvwFA1er.SubItems.Add(dt.Rows[i]["File_Type"].ToString());
+                    lvwFA1er.SubItems.Add(dt.Rows[i]["VOLUME_NO"].ToString());
+                    lvwFA1er.SubItems.Add(dt.Rows[i]["DeptName"].ToString());
+                    lvwFA1er.SubItems.Add(dt.Rows[i]["SELF_POSITION"].ToString());
+                    lvwFA1er.SubItems.Add(dt.Rows[i]["Start_Date"].ToString());
+                    lvwFA1er.SubItems.Add(dt.Rows[i]["End_Date"].ToString());
+                    lvwFA1er.SubItems.Add(dt.Rows[i]["MName"].ToString());
+                    lvwFA1er.SubItems.Add(dt.Rows[i]["Buyer_Name"].ToString());
+                    listView1.Items.Add(lvwFA1er);
+
+                }
+            }
+            catch
+            {
+            }
+        
+        }
+    }
+}
